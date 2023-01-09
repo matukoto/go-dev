c6f108acbdb891ac5da51e0f5bbf2deb39c07391
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2023-01-09 18:14:36 +0900

    fizbuz

--- for.go
+++ for.go
@@ -6,4 +6,41 @@ func main() {
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
 	}
+
+	// Go では if を短くすることが推奨されている
+	// ネストを浅くすることが推奨されている
+
+	// NG
+	// ネストが深く、また if 文の中身も長い
+	for i := 0; i <= 100; i++ {
+		if i%3 == 0 {
+			if i%5 == 0 {
+				fmt.Println(i, "FizzBuzz")
+			} else {
+				fmt.Println(i, "Fizz")
+			}
+		} else if i%5 == 0 {
+			fmt.Println(i, "Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+
+	// OK (推奨)
+	for i := 0; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println(i, "FizzBuzz")
+			continue
+		}
+		if i%3 == 0 {
+			fmt.Println(i, "Fizz")
+			continue
+		}
+		if i%5 == 0 {
+			fmt.Println(i, "Buzz")
+			continue
+		}
+		fmt.Println(i)
+	}
+
 }
