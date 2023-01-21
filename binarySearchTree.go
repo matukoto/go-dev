64143a173afcc95c4f2940a202cfd0e8b90865d9
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2023-01-21 22:05:37 +0900

    二分探索木を実装してみる

---
+++ binarySearchTree.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("a")
+}
+
+// 探索対象の配列
+data := [7]int{1, 3, 7, 13, 17, 21, 74}
+
+func linerSearch(int[] data, int key) int, int, int {
+  position := 0
+  end := len(data) - 1
+  steps := 1
+  for 
+}
+
