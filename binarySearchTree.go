42389fec49680c90a101e3c1f18f87c1a551d8e7
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2023-01-22 13:23:54 +0900

    feat: liner search 線形探索の実装
    python の線形探索を参考に golang で線形探索を書いてみた

--- binarySearchTree.go
+++ binarySearchTree.go
@@ -3,16 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("a")
+	// 探索対象の配列
+	data := []int{1, 3, 7, 13, 17, 21, 74}
+	steps, key, _ := linerSearch(data, 21)
+	fmt.Println(steps, key)
 }
 
-// 探索対象の配列
-data := [7]int{1, 3, 7, 13, 17, 21, 74}
+// 線形検索
+func linerSearch(data []int, key int) (int, int, error) {
+	position := 0
+	end := len(data) - 1
+	steps := 1
 
-func linerSearch(int[] data, int key) int, int, int {
-  position := 0
-  end := len(data) - 1
-  steps := 1
-  for 
+	for position < end {
+		if data[position] == key {
+			return steps, key, nil
+		}
+		position++
+		steps++
+	}
+	return steps, 0, nil
 }
-
